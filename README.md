a9bb5ba5ce4a96defd7563fdd25a123b77920775
David Curran <david.curran@ftr.com>
2018-05-23 12:40:36 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+A simple CLI emulator to use when playing with Blue Planet. It has all of the CRUDL functions and a "show device" type function
+
+```
+currand@M_C02N72W0G3QK_fake$ help
+
+Documented commands (type help <topic>):
+========================================
+help
+
+Undocumented commands:
+======================
+create_item  delete_item  exit  list_items  read_item  show_device  update_item
+
+currand@M_C02N72W0G3QK_fake$ exit
+Goodbye...
+currand@M_C02N72W0G3QK ~/code/cli_emulator $ atom Readme.md
+currand@M_C02N72W0G3QK ~/code/cli_emulator $ python cli_emulator.py
+Test shell
+currand@M_C02N72W0G3QK_fake$ help
+
+Documented commands (type help <topic>):
+========================================
+help
+
+Undocumented commands:
+======================
+create_item  delete_item  exit  list_items  read_item  show_device  update_item
+
+currand@M_C02N72W0G3QK_fake$ list_items
+0: spam
+1: ham
+2: eggs
+currand@M_C02N72W0G3QK_fake$ read_item 2
+2:eggs
+currand@M_C02N72W0G3QK_fake$ show_device
+OS: Darwin
+Release: 16.7.0
+Version: Darwin Kernel Version 16.7.0: Mon Nov 13 21:56:25 PST 2017; root:xnu-3789.72.11~1/RELEASE_X86_64
+Machine: x86_64
+Processori386
+currand@M_C02N72W0G3QK_fake$
+```
---
+++ cli_emulator.py
@@ -0,0 +1,71 @@
+import cmd
+import sys
+import getpass
+import socket
+import shlex
+import platform
+
+items = [
+    "spam",
+    "ham",
+    "eggs"
+]
+
+port = 8123
+
+class Shell(cmd.Cmd):
+    intro = "Test shell"
+    prompt = getpass.getuser() + "@" + socket.gethostname() + "_fake$ "
+
+    def print_items(self):
+        for i in range(0, len(items)):
+            print(str(i) + ": " + items[i])
+
+    def do_list_items(self, args):
+        self.print_items()
+
+    def do_create_item(self, args):
+        items.append(args)
+        self.print_items()
+
+    def do_update_item(self, args):
+        try:
+            key, value = args.split(':')
+            key = int(key)
+        except (TypeError, ValueError):
+            print("Enter item in the format:\n  <item number>:<item>\n  e.g. 3:spam")
+            return False
+
+        try:
+            items[key] = value
+            print(str(key) + ":" + value)
+        except IndexError:
+            print("Item does not exist")
+
+    def do_read_item(self, args):
+        try:
+            print(args + ":" + items[int(args)])
+        except (IndexError, ValueError):
+            print("Item does not exist")
+
+    def do_delete_item(self, args):
+        try:
+            del items[int(args)]
+        except IndexError:
+            print("Item does not exist")
+
+    def do_show_device(self, args):
+        print("OS: " + platform.system())
+        print("Release: " + platform.release())
+        print("Version: " + platform.version())
+        print("Machine: " + platform.machine())
+        print("Processor" + platform.processor())
+
+
+    def do_exit(self, arg):
+        print("Goodbye...")
+        sys.exit()
+
+
+if __name__ == '__main__':
+    Shell().cmdloop()
