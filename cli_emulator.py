7eaf216cc1dcb1e45abbaae86de941cf7ff59551
David Curran <david.curran@ftr.com>
2018-05-23 13:07:47 -0400

    Added docs

--- cli_emulator.py
+++ cli_emulator.py
@@ -1,4 +1,3 @@
-import future
 import cmd
 import sys
 import getpass
@@ -6,6 +5,7 @@ import socket
 import shlex
 import platform
 
+version = "0.9"
 items = [
     "spam",
     "ham",
@@ -15,21 +15,25 @@ items = [
 port = 8123
 
 class Shell(cmd.Cmd):
-    intro = "Test shell"
+    intro = "\n\nTest shell " + version + "\n by: David Curran (david.curran@ftr.com)\n Type 'help' to see a list of commands\n"
     prompt = getpass.getuser() + "@" + socket.gethostname() + "_fake$ "
+    completekey='tab'
 
     def print_items(self):
         for i in range(0, len(items)):
             print(str(i) + ": " + items[i])
 
     def do_list_items(self, args):
+        'List items'
         self.print_items()
 
     def do_create_item(self, args):
+        'Create an item: create_item <item>'
         items.append(args)
         self.print_items()
 
     def do_update_item(self, args):
+        'Update item: update_item <index>:<item> (e.g. \'3:spam\' )'
         try:
             key, value = args.split(':')
             key = int(key)
@@ -44,18 +48,21 @@ class Shell(cmd.Cmd):
             print("Item does not exist")
 
     def do_read_item(self, args):
+        'Read a single item: read_item <index>'
         try:
             print(args + ":" + items[int(args)])
         except (IndexError, ValueError):
             print("Item does not exist")
 
     def do_delete_item(self, args):
+        'Delete an item: delete_item <index>'
         try:
             del items[int(args)]
         except IndexError:
             print("Item does not exist")
 
     def do_show_device(self, args):
+        "Show system info"
         print("OS: " + platform.system())
         print("Release: " + platform.release())
         print("Version: " + platform.version())
@@ -67,6 +74,8 @@ class Shell(cmd.Cmd):
         print("Goodbye...")
         sys.exit()
 
+    def postcmd(self, stop, line):
+        print()
 
 if __name__ == '__main__':
     Shell().cmdloop()
