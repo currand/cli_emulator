be603725e472c061249a8748a769039197800627
David Curran <david.curran@ftr.com>
2018-05-23 16:01:10 -0400

    Added resource commands

--- cli_emulator.py
+++ cli_emulator.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 import cmd
 import sys
 import getpass
@@ -6,9 +8,18 @@ import platform
 
 version = "0.9"
 items = [
-    "spam",
-    "ham",
-    "eggs"
+    {
+        "name": "spam",
+        "resources": ["resource 1"]
+    },
+    {
+        "name": "ham",
+        "resources": ["resource 1"]
+    },
+    {
+        "name": "eggs",
+        "resources": ["resource 1"]
+    }
 ]
 
 
@@ -22,13 +33,24 @@ class Shell(cmd.Cmd):
 
     def print_items(self):
         for i in range(0, len(items)):
-            print(str(i) + ": " + items[i])
+            print(str(i) + ": " + items[i]['name'])
 
     def print_item(self, index):
         if index is 'last':
-            print(str(len(items)-1) + ": " + items[-1])
+            print(str(len(items)-1) + ": " + items[-1]['name'])
         else:
-            print(str(index) + ": " + items[index])
+            print(str(index) + ": " + items[index]['name'])
+
+    def print_resources(self, item):
+        if len(items[item]['resources']) is 0:
+            print("Error: Item has no resources")
+            return
+        for i, resource in enumerate(items[item]["resources"]):
+            print("  " + str(i) + ": " + resource)
+
+    def print_resource(self, item, resource):
+        print("  " + str(item) + ": " + items[item]["resources"][resource])
+
 
     def do_list_items(self, args):
         'List items'
@@ -36,6 +58,10 @@ class Shell(cmd.Cmd):
 
     def do_create_item(self, args):
         'Create an item: create_item <item>'
+        item = {
+            "name": args,
+            "resources": []
+        }
         items.append(args)
         self.print_item('last')
 
@@ -59,6 +85,8 @@ class Shell(cmd.Cmd):
         'Read a single item: read_item <index>'
         try:
             self.print_item(int(args))
+            self.print_resources(int(args))
+
         except (IndexError, ValueError):
             print("Error: Item does not exist")
 
@@ -69,6 +97,64 @@ class Shell(cmd.Cmd):
             print("OK")
         except IndexError:
             print("Error: Item does not exist")
+
+    def do_create_resource(self, args):
+        'Create a resource: create_resource <item index>: <resource>\n (e.g. 3: resource1)'
+        try:
+            item, new_resource = args.split(':')
+            items[int(item)]['resources'].append(new_resource)
+            print("OK")
+        except IndexError:
+            print("Error: Item does not exist")
+        except (ValueError, TypeError):
+            print ('Create a resource: create_resource <item index>: <resource>\n (e.g. 3: resource1)')
+
+
+    def do_read_resource(self, args):
+        'Read a resource: read_resource <item index>:<resource index>\n (e.g. 3:2)'
+        try:
+            item, resource = args.split(':')
+            print("OK")
+            self.print_resource(int(item), int(resource))
+        except IndexError:
+            print("Error: Item does not exist")
+        except (ValueError, TypeError):
+            print ('Create a resource: create_resource <item index>: <resource>\n (e.g. 3: resource1)')
+
+
+    def do_update_resource(self, args):
+        'Update a resource: update_resource <item index>:<resource index>:<resource>\n (e.g. 3:2:bob)'
+        try:
+            item, resource, update = args.split(':')
+            items[int(item)]['resources'][int(resource)] = update
+            print("OK")
+            self.print_resource(int(item), int(resource))
+        except IndexError:
+            print("Error: Item or resource does not exist")
+        except (ValueError, TypeError):
+            print ('Update a resource: update_resource <item index>:<resource index>:<resource>\n (e.g. 3:2:bob)')
+
+
+    def do_delete_resource(self, args):
+        'Delte a resource: delete_resource <item index>:<resource index>\n (e.g. 3:2)'
+        try:
+            item, resource = args.split(':')
+            del items[int(item)]['resources'][int(resource)]
+            print("OK")
+        except IndexError:
+            print("Error: Item or resource does not exist")
+        except (ValueError, TypeError):
+            print ('Delete a resource: delete_resource <item index>:<resource index>\n (e.g. 3:2)')
+
+    def do_list_resources(self, args):
+        'List resources: list_resources <item index>'
+        try:
+            self.print_resources(int(args))
+        except IndexError:
+            print("Error: Item does not exist")
+        except (ValueError, TypeError):
+            print ('Create a resource: create_resource <item index>: <resource>\n (e.g. 3: resource1)')
+
 
     def do_show_device(self, args):
         "Show system info"
