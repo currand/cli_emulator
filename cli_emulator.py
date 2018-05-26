7402e3f5d322c62e57295b2234f14feb8f2b5283
David Curran <david.curran@ftr.com>
2018-05-26 16:32:40 -0400

    Complete change to router model

--- cli_emulator.py
+++ cli_emulator.py
@@ -1,26 +1,99 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 
 import cmd
 import sys
 import getpass
 import socket
 import platform
+import re
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+jinja_env = Environment(
+    loader=PackageLoader('cli_emulator', 'templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+    )
 
 version = "0.9"
-items = [
-    {
-        "name": "spam",
-        "resources": ["resource 1"]
+
+ipadd_pattern = re.compile('\b(?:\d{1,3}\.){3}\d{1,3}\b')
+
+interfaces = {
+    "ge-1/0/2": {
+        "description": "CKT ETH/12345/A",
+        "sub_interfaces": {
+            "10": {
+                "description": "Bob's Taco Shack Site 1 - CKT VC/123",
+                "ip_address": "192.168.2.1/24",
+                "type": "Gigabit Ethernet",
+                "vlan": 10,
+                "proto_state": "up",
+                "int_state": "up",
+                "bandwidth": "5000M"
+            },
+            "11": {
+                "description": "Bob's Taco Shack Site 2 - CKT VC/124",
+                "ip_address": "192.168.3.1/24",
+                "type": "Gigabit Ethernet",
+                "vlan": 11,
+                "proto_state": "up",
+                "int_state": "up",
+                "bandwidth": "5000M"
+            }
+        }
     },
-    {
-        "name": "ham",
-        "resources": ["resource 1"]
+    "ge-1/0/1": {
+        "description": "CKT ETH/45678/QZ",
+        "sub_interfaces": {
+            "10": {
+                "description": "Bob's Taco Shack Host - CKT VC/125",
+                "ip_address": "192.168.1.1/24",
+                "type": "Gigabit Ethernet",
+                "vlan": 10,
+                "proto_state": "down",
+                "int_state": "down",
+                "bandwidth": "10000M"
+            }
+        }
     },
-    {
-        "name": "eggs",
-        "resources": ["resource 1"]
+    "ge-2/0/0": {
+        "description": "Uplink CKT GE/1212345/QZDW",
+        "ip_address": "192.168.4.1/24",
+        "type": "Ten-Gigabit Ethernet",
+        "vlan": 1000,
+        "proto_state": "up",
+        "int_state": "up",
+        "bandwidth": "100000M"
+    },
+    "loopback0": {
+        "description": "BGP Peer Interface",
+        "ip_address": "192.168.5.1/32",
+        "type": "Loopback",
+        "vlan": 0,
+        "proto_state": "up",
+        "int_state": "up",
+        "bandwidth": "100000M"
     }
-]
+}
+
+xconnects = {
+    "vpn1": {
+        "source-int": "ge-1/0/1.10",
+        "dest-int": "ge-1/0/2.10",
+        "type": "vlan",
+    },
+    "vpn2": {
+        "source-int": "ge-1/0/1.11",
+        "dest-int": "ge-1/0/2.10",
+        "type": "vlan",
+    },
+    "offnet-vpn": {
+        "source-int": "10.1.1.1",
+        "dest-int": "ge-1/0/2.10",
+        "type": "mpls"
+    }
+}
+
+xconnect_hash = {}
 
 
 class Shell(cmd.Cmd):
@@ -31,125 +104,93 @@ class Shell(cmd.Cmd):
     prompt = getpass.getuser() + "@" + socket.gethostname() + "_fake$ "
     completekey = 'tab'
 
-    def print_items(self):
-        for i in range(0, len(items)):
-            print(str(i) + ": " + items[i]['name'])
+    def update_xconnect_hash(self):
+        for xconnect in xconnects:
+            xconnect_hash[xconnect["source-int"]] = xconnect["dest-int"]
+            xconnect_hash[xconnect["dest-int"]] = xconnect["source-int"]
 
-    def print_item(self, index):
-        if index is 'last':
-            print(str(len(items)-1) + ": " + items[-1]['name'])
-        else:
-            print(str(index) + ": " + items[index]['name'])
+    def check_for_int(self, int):
+        if int in interfaces.keys():
+            print("True")
+            return True
 
-    def print_resources(self, item):
-        if len(items[item]['resources']) is 0:
-            print("Error: Item has no resources")
-            return
-        for i, resource in enumerate(items[item]["resources"]):
-            print("  " + str(i) + ": " + resource)
-
-    def print_resource(self, item, resource):
-        print("  " + str(item) + ": " + items[item]["resources"][resource])
-
-    def do_list_items(self, args):
-        'List items'
-        self.print_items()
-
-    def do_create_item(self, args):
-        'Create an item: create_item <item>'
-        item = {
-            "name": args.strip(' \t\r\n'),
-            "resources": []
-        }
-        items.append(item)
-        self.print_item('last')
-
-    def do_update_item(self, args):
-        'Update item: update_item <index>:<item> (e.g. \'3:spam\' )'
         try:
-            key, value = args.split(':')
-            key = int(key)
-        except (TypeError, ValueError):
-            print("""Error: Enter item in the format:
-                  \n  <item number>:<item>\n  e.g. 3:spam""")
+            int, sub_int = int.split('.')
+            if sub_int in interfaces[int]['sub_interfaces'].keys():
+                print("True")
+                return True
+            else:
+                print("False")
+                return False
+        except (ValueError, KeyError):
+            print("False")
             return False
 
+    def do_create_xconnect(self, args):
+        'create_xconnect <source> <dest> <type>'
         try:
-            items[key] = value.strip(' \t\r\n')
-            self.print_item(key)
-        except IndexError:
-            print("Error: Item does not exist")
+            source, dest, type, name = args.split('\s+')
+        except ValueError:
+            'create_xconnect <source> <dest> <type> <name>'
 
-    def do_read_item(self, args):
-        'Read a single item: read_item <index>'
+        if not self.check_for_int(source) or self.check_for_int(dest):
+            if not ipadd_pattern.match(source) or not ipadd_pattern(dest):
+                print('source/destination must be valid interface or IP Address')
+                return False
+
+        if type not in ['mpls', 'vlan']:
+            print('type must be one of [vlan, mpls]')
+            return False
+
+        xconnects[name] = {
+            "source-int": source,
+            "dest-int": dest,
+            "type": type
+        }
+
+        self.update_xconnect_hash
+        print(name)
+        return True
+
+    def do_show_xconnects(self, args):
+        template = jinja_env.get_template('show-xconnects.tmpl')
+        print(template.render(xconnects=xconnects))
+
+        return True
+
+
+    def do_show_interface(self, args):
+        'do_show_interface <interface>.[sub-interface]'
+
+        sub_int = False
         try:
-            self.print_item(int(args))
-            self.print_resources(int(args))
+            if '.' in args:
+                int, sub_int = args.split('.')
+            else:
+                int = args
+        except ValueError:
+            'do_show_interface <interface>[.sub-interface]'
 
-        except (IndexError, ValueError):
-            print("Error: Item does not exist")
+        if int not in interfaces.keys():
+            print("Interface {} not found".format(int))
+            return False
+        if sub_int and sub_int not in interfaces[int]["sub_interfaces"].keys():
+            print("Interface {}.{} not found".format(int, sub_int))
+            return False
 
-    def do_delete_item(self, args):
-        'Delete an item: delete_item <index>'
-        try:
-            del items[int(args)]
-            print("OK")
-        except IndexError:
-            print("Error: Item does not exist")
+        template = jinja_env.get_template('show_interface.tmpl')
+        print(template.render(int=int, sub_int=sub_int, interfaces=interfaces))
+        return True
 
-    def do_create_resource(self, args):
-        'Create a resource: create_resource <item index>: <resource>\n (e.g. 3: resource1)'
-        try:
-            item, new_resource = args.split(':')
-            items[int(item)]['resources'].append(new_resource.strip(' \t\r\n'))
-            print("OK")
-        except IndexError:
-            print("Error: Item does not exist")
-        except (ValueError, TypeError):
-            print('Create a resource: create_resource <item index>: <resource>\n (e.g. 3: resource1)')
+    def do_show_interfaces(self, args):
+        'Show all interfaces'
+        template = jinja_env.get_template('show_interface.tmpl')
 
-    def do_read_resource(self, args):
-        'Read a resource: read_resource <item index>:<resource index>\n (e.g. 3:2)'
-        try:
-            item, resource = args.split(':')
-            print("OK")
-            self.print_resource(int(item), int(resource))
-        except IndexError:
-            print("Error: Item does not exist")
-        except (ValueError, TypeError):
-            print('Create a resource: create_resource <item index>: <resource>\n (e.g. 3: resource1)')
+        for int in interfaces:
+            print(template.render(int=int, sub_int=False,
+                                  interfaces=interfaces))
 
-    def do_update_resource(self, args):
-        'Update a resource: update_resource <item index>:<resource index>:<resource>\n (e.g. 3:2:bob)'
-        try:
-            item, resource, update = args.split(':')
-            items[int(item)]['resources'][int(resource)] = update.strip(' \t\r\n')
-            print("OK")
-            self.print_resource(int(item), int(resource))
-        except IndexError:
-            print("Error: Item or resource does not exist")
-        except (ValueError, TypeError):
-            print('Update a resource: update_resource <item index>:<resource index>:<resource>\n (e.g. 3:2:bob)')
-
-    def do_delete_resource(self, args):
-        'Delte a resource: delete_resource <item index>:<resource index>\n (e.g. 3:2)'
-        try:
-            item, resource = args.split(':')
-            del items[int(item)]['resources'][int(resource)]
-            print("OK")
-        except IndexError:
-            print("Error: Item or resource does not exist")
-        except (ValueError, TypeError):
-            print('Delete a resource: delete_resource <item index>:<resource index>\n (e.g. 3:2)')
-
-    def do_list_resources(self, args):
-        'List resources: list_resources <item index>'
-        try:
-            self.print_resources(int(args))
-        except IndexError:
-            print("Error: Item does not exist")
-        except (ValueError, TypeError):
-            print('Create a resource: create_resource <item index>: <resource>\n (e.g. 3: resource1)')
+        return True
 
     def do_show_device(self, args):
         "Show system info"
